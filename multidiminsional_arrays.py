a200f5af15e1c1ddfef8275e3a092b2db2ff42a5
Yashik dhiman <yashikk507@gmail.com>
2026-06-22 22:02:08 +0530

    code of multidimension array

--- multidiminsional_arrays.py
+++ multidiminsional_arrays.py
@@ -1,1 +1,7 @@
 import numpy as np
+arr1 = np.array([[1,2,3,4],[6,7,8,9],[2,3,7,8]])
+print("type of the array",arr1.dtype)
+print("array:",arr1)
+print("shape of the array:",arr1.shape)
+print("dimension of the array:",arr1.ndim)
+print("no. off elemnts in the array",arr1.size)
