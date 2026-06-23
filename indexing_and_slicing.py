76c39c29713cae0ff286dc222bba9cc29b40cc72
Yashik dhiman <yashikk507@gmail.com>
2026-06-23 13:43:48 +0530

    how to acess elemnts by indeces

---
+++ indexing_and_slicing.py
@@ -0,0 +1,6 @@
+import numpy as np
+matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(matrix)
+print("element at row 1 and column 2:",matrix[1,2])
+print("first row",matrix[0,:])
+print("second column",matrix[:,1])
